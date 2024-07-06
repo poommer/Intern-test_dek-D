8a37131729ba18e77729d9a408a2094d01f95753
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2024-07-06 16:32:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ session_start();
                     <label for="title">เนื้อหากระทู้<span><small id="content-invalid" style="color:red;"></small></span></label>
                     <div style="display: flex; align-items: center;">
                     <svg style="display: none;" id="icon-content" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00ad23"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
-                    <label id="numberCharContent">20</label>
+                    <label id="numberCharContent">2000</label>
                 </div>
                 </div>
                 <textarea id="content" name="content" placeholder="เนื้อหากระทู้" maxlength="2060"></textarea>
